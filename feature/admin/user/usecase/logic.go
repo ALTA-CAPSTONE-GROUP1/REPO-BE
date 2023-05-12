1643f59b83c552823c018cd8b8c9f631305188d0
unknown <adiyudapranata@gmail.com>
2023-05-13 02:34:18 +0700

    feat(AdminUser): create register logic

--- feature/admin/user/usecase/logic.go
+++ feature/admin/user/usecase/logic.go
@@ -1,1 +1,35 @@
 package usecase
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/admin/user"
+	"github.com/labstack/gommon/log"
+)
+
+type userLogic struct {
+	u user.Repository
+}
+
+func New(u user.Repository) user.UseCase {
+	return &userLogic{
+		u: u,
+	}
+}
+
+// RegisterUser implements user.UseCase
+func (ul *userLogic) RegisterUser(newUser user.Core) error {
+	if err := ul.u.InsertUser(newUser); err != nil {
+		log.Error("error on calling register insert user query", err.Error())
+		if strings.Contains(err.Error(), "column") {
+			return errors.New("server error")
+		} else if strings.Contains(err.Error(), "value") {
+			return errors.New("invalid value")
+		} else if strings.Contains(err.Error(), "too short") {
+			return errors.New("invalid password length")
+		}
+		return errors.New("server error")
+	}
+	return nil
+}
