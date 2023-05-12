b3db1c4bc1aaa1e8e23873dbd157ec5968245b8d
Kristain Putra <kputrapar@gmail.com>
2023-05-12 10:53:49 +0700

    build(Models): Create database tables using gorm and auto mig
    Closes #19

--- feature/submission/repository/model.go
+++ feature/submission/repository/model.go
@@ -1,1 +1,56 @@
 package repository
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Submission struct {
+	ID        int    `gorm:"primaryKey;autoIncrement"`
+	UserID    string `gorm:"size:50"`
+	TypeID    int    `gorm:"not null"`
+	Title     string `gorm:"size:50;not null"`
+	Message   string `gorm:"type:text;not null"`
+	Status    string `gorm:"size:50;not null"`
+	Is_Opened bool
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt
+	Files     []File
+	Tos       []To
+	Ccs       []Cc
+	Signs     []Sign
+}
+
+type File struct {
+	ID           int `gorm:"primaryKey;autoIncrement"`
+	SubmissionID int
+	Name         string `gorm:"size:50;not null"`
+	Link         string `gorm:"size:50;not null"`
+}
+
+type Cc struct {
+	ID           int `gorm:"primaryKey;autoIncrement"`
+	SubmissionID int
+	UserID       string `gorm:"size:50"`
+	Name         string `gorm:"size:50;not null"`
+	Is_Opened    bool
+	CreatedAt    time.Time `gorm:"autoCreateTime"`
+}
+
+type To struct {
+	ID           int `gorm:"primaryKey;autoIncrement"`
+	SubmissionID int
+	UserID       string `gorm:"size:50"`
+	Name         string `gorm:"size:50;not null"`
+	Action_Type  string `gorm:"size:50;not null"`
+	Is_Opened    bool
+	CreatedAt    time.Time `gorm:"autoCreateTime"`
+}
+
+type Sign struct {
+	ID           int `gorm:"primaryKey"`
+	SubmissionID int
+	CreatedAt    time.Time `gorm:"autoCreateTime"`
+}
