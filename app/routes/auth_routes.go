fafc7600c594f23ebb3926cf840d49cc3c9d538a
unknown <adiyudapranata@gmail.com>
2023-05-15 06:40:08 +0700

    feat(Routes): create auth login routes

--- app/routes/auth_routes.go
+++ app/routes/auth_routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/auth"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func AuthRoutes(e *echo.Echo, uc auth.Handler) {
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.POST("/login", uc.LoginHandler())
+
+}
