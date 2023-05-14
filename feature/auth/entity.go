997feee657e999b6ba4c2349819ec1b872490fe9
unknown <adiyudapranata@gmail.com>
2023-05-15 06:27:32 +0700

    chore(Auth): create login interface

--- feature/auth/entity.go
+++ feature/auth/entity.go
@@ -1,1 +1,33 @@
-package user
+package auth
+
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+type Core struct {
+	ID          string
+	OfficeID    int
+	PositionID  int
+	Name        string
+	Email       string
+	PhoneNumber string
+	Password    string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt
+}
+
+type Handler interface {
+	LoginHandler() echo.HandlerFunc
+}
+
+type UseCase interface {
+	LogInLogic(id string, password string) (Core, error)
+}
+
+type Repository interface {
+	Login(id string, password string) (Core, error)
+}
