1d20c9a07879ea70ec0f4d854319fa27b77a2bdc
unknown <adiyudapranata@gmail.com>
2023-05-15 06:29:37 +0700

    chore(Auth): create login repository

--- feature/auth/repository/model.go
+++ feature/auth/repository/model.go
@@ -1,1 +1,20 @@
 package repository
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID          string         `gorm:"primaryKey;size:50"`
+	OfficeID    int            `gorm:"foreignKey:OfficeID"`
+	PositionID  int            `gorm:"foreignKey:PositionID"`
+	Name        string         `gorm:"size:50;not null"`
+	Email       string         `gorm:"size:50"`
+	PhoneNumber string         `gorm:"size:50"`
+	Password    string         `gorm:"size:50"`
+	CreatedAt   time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
--- feature/auth/repository/query.go
+++ feature/auth/repository/query.go
@@ -1,1 +1,41 @@
 package repository
+
+import (
+	"errors"
+
+	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/auth"
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+type authModel struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) auth.Repository {
+	return &authModel{
+		db: db,
+	}
+}
+
+// Login implements auth.Repository
+func (um *authModel) Login(id string, password string) (auth.Core, error) {
+	input := User{}
+
+	if id == "" {
+		log.Error("id login is blank")
+		return auth.Core{}, errors.New("data does not exist")
+	}
+
+	if err := um.db.Where("id = ?", id).First(&input).Error; err != nil {
+		log.Error("error occurs on select users login", err.Error())
+		return auth.Core{}, err
+	}
+
+	// if err := helper.VerifyPassword(input.Password, password); err != nil {
+	// 	log.Error("user input for password is wrong", err.Error())
+	// 	return auth.Core{}, errors.New("wrong password")
+	// }
+
+	return auth.Core{}, nil
+}
