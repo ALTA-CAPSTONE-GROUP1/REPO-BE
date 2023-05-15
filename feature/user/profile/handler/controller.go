9abd75edffcb1ca26f2a5c10fee0ec5b5720094a
unknown <adiyudapranata@gmail.com>
2023-05-16 00:06:31 +0700

    feat(User): create user profile handler

--- feature/user/profile/handler/controller.go
+++ feature/user/profile/handler/controller.go
@@ -1,1 +1,33 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/user/profile"
+	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/helper"
+	"github.com/labstack/echo/v4"
+)
+
+type profileController struct {
+	service profile.UseCase
+}
+
+func New(u profile.UseCase) profile.Handler {
+	return &profileController{
+		service: u,
+	}
+}
+
+// ProfileHandler implements profile.Handler
+func (uc *profileController) ProfileHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		userID := helper.DecodeToken(c)
+		data, err := uc.service.ProfileLogic(userID)
+		if err != nil {
+			c.Logger().Error("error on calling user profile logic")
+			return c.JSON(helper.ResponseFormat(http.StatusInternalServerError, "server error", nil))
+		}
+		dataResponse := CoreToUserResponse(data)
+		return c.JSON(helper.ResponseFormat(http.StatusOK, "succes to get user profile", dataResponse))
+	}
+}
--- feature/user/profile/handler/response.go
+++ feature/user/profile/handler/response.go
@@ -1,1 +1,19 @@
 package handler
+
+import "github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/user/profile"
+
+type UserResponse struct {
+	ID          string `json:"user_id"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+}
+
+func CoreToUserResponse(data profile.Core) UserResponse {
+	return UserResponse{
+		ID:          data.ID,
+		Name:        data.Name,
+		Email:       data.Email,
+		PhoneNumber: data.PhoneNumber,
+	}
+}
