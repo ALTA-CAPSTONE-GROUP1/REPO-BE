7c07ce33bf8d45c5957470c9dc229e23eac1e951
unknown <adiyudapranata@gmail.com>
2023-05-15 11:09:10 +0700

    chore(AdminUser): fix register user for auto password

--- feature/admin/user/handler/request.go
+++ feature/admin/user/handler/request.go
@@ -6,14 +6,12 @@ type RegisterInput struct {
 	Name        string `json:"name"`
 	Email       string `json:"email"`
 	PhoneNumber string `json:"phone_number"`
-	Password    string `json:"password"`
 }
 
 type InputUpdate struct {
 	Name        string `json:"name" form:"name"`
 	Email       string `json:"email" form:"email"`
 	PhoneNumber string `json:"phone_number" form:"phone_number"`
-	Password    string `json:"password" form:"password"`
 	OfficeID    int    `json:"office_id" form:"office_id"`
 	PositionID  int    `json:"position_id"  form:"position_id"`
 }
--- feature/auth/repository/query.go
+++ feature/auth/repository/query.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/auth"
+	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/helper"
 	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
 )
@@ -32,10 +33,24 @@ func (um *authModel) Login(id string, password string) (auth.Core, error) {
 		return auth.Core{}, err
 	}
 
-	// if err := helper.VerifyPassword(input.Password, password); err != nil {
-	// 	log.Error("user input for password is wrong", err.Error())
-	// 	return auth.Core{}, errors.New("wrong password")
-	// }
+	// Check if the user is the admin and the password is "admin"
+	if input.ID == "admin" && password == "admin" {
+		return auth.Core{
+			ID:          input.ID,
+			OfficeID:    input.OfficeID,
+			PositionID:  input.PositionID,
+			Name:        input.Name,
+			Email:       input.Email,
+			PhoneNumber: input.PhoneNumber,
+			Password:    input.Password,
+		}, nil
+	}
+
+	// Perform the regular password verification for non-admin users
+	if err := helper.VerifyPassword(input.Password, password); err != nil {
+		log.Error("user input for password is wrong", err.Error())
+		return auth.Core{}, errors.New("wrong password")
+	}
 
 	return auth.Core{
 		ID:          input.ID,
