83eb4a5b0ae43325bd269c745e5f7c36ec0664a7
unknown <adiyudapranata@gmail.com>
2023-05-16 00:28:40 +0700

    feat(User): create user update handler

--- feature/user/profile/handler/controller.go
+++ feature/user/profile/handler/controller.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/user/profile"
 	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/helper"
+	"github.com/jinzhu/copier"
 	"github.com/labstack/echo/v4"
 )
 
@@ -15,6 +17,35 @@ type profileController struct {
 func New(u profile.UseCase) profile.Handler {
 	return &profileController{
 		service: u,
+	}
+}
+
+// UpdateUserHandler implements profile.Handler
+func (uc *profileController) UpdateUserHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		userID := helper.DecodeToken(c)
+		updateInput := InputUpdate{}
+		if err := c.Bind(&updateInput); err != nil {
+			c.Logger().Error("terjadi kesalahan bind", err.Error())
+			return c.JSON(helper.ResponseFormat(http.StatusBadRequest, "invalid input", nil))
+		}
+
+		updateUser := profile.Core{}
+		copier.Copy(&updateUser, &updateInput)
+		if err := uc.service.UpdateUser(userID, updateUser); err != nil {
+			c.Logger().Error("failed on calling updateprofile log")
+			if strings.Contains(err.Error(), "hashing password") {
+				c.Logger().Error("hashing password error")
+				return c.JSON(helper.ResponseFormat(http.StatusBadRequest, "new password are invalid", nil))
+			} else if strings.Contains(err.Error(), "affected") {
+				c.Logger().Error("no rows affected on update user")
+				return c.JSON(helper.ResponseFormat(http.StatusBadRequest, "data is up to date", nil))
+			}
+
+			return c.JSON(helper.ResponseFormat(http.StatusInternalServerError, "internal server error", nil))
+		}
+
+		return c.JSON(helper.ResponseFormat(http.StatusOK, "succes to update user data", nil))
 	}
 }
 
--- feature/user/profile/handler/request.go
+++ feature/user/profile/handler/request.go
@@ -1,1 +1,7 @@
 package handler
+
+type InputUpdate struct {
+	Email       string `json:"email" form:"email"`
+	PhoneNumber string `json:"phone_number" form:"phone_number"`
+	Password    int    `json:"password" form:"password"`
+}
