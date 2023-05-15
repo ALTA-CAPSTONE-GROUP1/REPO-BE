6dd8b96ca1686c21031cf4024cde4a52b79663ce
unknown <adiyudapranata@gmail.com>
2023-05-15 23:29:27 +0700

    chore(User): create user model and user profile interface

---
+++ feature/user/profile/entity.go
@@ -0,0 +1,33 @@
+package profile
+
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+type Core struct {
+	ID          string
+	OfficeID    int
+	PositionID  int
+	Name        string
+	Email       string
+	PhoneNumber string
+	Password    string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt
+}
+
+type Handler interface {
+	LoginHandler() echo.HandlerFunc
+}
+
+type UseCase interface {
+	LogInLogic(id string, password string) (Core, error)
+}
+
+type Repository interface {
+	Login(id string, password string) (Core, error)
+}
