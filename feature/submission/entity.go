5a0ae98278c038eb906f775a64f8a440458ba08a
Kristain Putra <kputrapar@gmail.com>
2023-05-16 21:40:33 +0700

    feat(GetSubmissions): query with all required data

--- feature/submission/entity.go
+++ feature/submission/entity.go
@@ -3,6 +3,7 @@ package submission
 import (
 	"mime/multipart"
 
+	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/admin"
 	"github.com/labstack/echo/v4"
 )
 
@@ -19,6 +20,26 @@ type UseCase interface {
 type Repository interface {
 	FindRequirement(userID string, typeName string, value int) (Core, error)
 	InsertSubmission(newSub AddSubmissionCore) error
+	SelectAllSubmissions(userID string, pr GetAllQueryParams) ([]AllSubmiisionCore, []admin.Type, error)
+}
+
+type AllSubmiisionCore struct {
+	ID             int
+	Tos            []ToApprover
+	CCs            []CcApprover
+	Title          string
+	Status         string
+	ReceiveDate    string
+	Opened         bool
+	Attachment     string
+	SubmissionType string
+}
+
+type GetAllQueryParams struct {
+	Title  string
+	To     string
+	Limit  int
+	Offset int
 }
 
 type AddSubmissionCore struct {
