f50702355d2284a3aa65b5646cfeb0e6bf8865ee
unknown <adiyudapranata@gmail.com>
2023-05-16 23:40:56 +0700

    feat(User): fix response profile

--- feature/user/profile/repository/query.go
+++ feature/user/profile/repository/query.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"errors"
 
+	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/admin/office"
+	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/admin/position"
 	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/user"
 	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/feature/user/profile"
 	"github.com/ALTA-CAPSTONE-GROUP1/e-proposal-BE/helper"
@@ -50,15 +52,33 @@ func (um *userModel) UpdateUser(id string, input profile.Core) error {
 
 // Profile implements profile.Repository
 func (um *userModel) Profile(id string) (profile.Core, error) {
-	tmp := profile.Core{}
-	tx := um.db.Table("users").Where("id = ?", id).First(&tmp)
-	if tx.RowsAffected < 1 {
-		log.Error("Terjadi error saat first user (data tidak ditemukan)")
-		return profile.Core{}, errors.New("user not found")
+	var dbuser user.Users
+	err := um.db.Table("users").
+		Preload("Position").
+		Preload("Office").
+		Joins("JOIN positions ON positions.id = users.position_id").
+		Joins("JOIN offices ON offices.id = users.office_id").
+		Select("users.id, users.office_id, users.position_id, users.name, users.email, users.phone_number, users.password, positions.name as position_name, offices.name as office_name").
+		Where("users.id = ?", id).
+		First(&dbuser).Error
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return profile.Core{}, errors.New("user not found")
+		}
+		log.Error("failed to find user:", err.Error())
+		return profile.Core{}, errors.New("failed to retrieve user")
 	}
-	if tx.Error != nil {
-		log.Error("Terjadi Kesalahan")
-		return profile.Core{}, tx.Error
-	}
-	return tmp, nil
+
+	return profile.Core{
+		ID:          dbuser.ID,
+		OfficeID:    dbuser.OfficeID,
+		PositionID:  dbuser.PositionID,
+		Name:        dbuser.Name,
+		Email:       dbuser.Email,
+		PhoneNumber: dbuser.PhoneNumber,
+		Password:    dbuser.Password,
+		Position:    position.Core{Name: dbuser.Position.Name},
+		Office:      office.Core{Name: dbuser.Office.Name},
+	}, nil
 }
