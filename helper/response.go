41b5a09436eaf63fb082efe0cfb90ee3017713ed
unknown <adiyudapranata@gmail.com>
2023-05-17 19:07:51 +0700

    fix(Admin): fix offset limit and pagination office and user

--- helper/response.go
+++ helper/response.go
@@ -1,5 +1,7 @@
 package helper
 
+import "math"
+
 func ResponseFormat(code int, msg string, data any) (int, map[string]any) {
 	res := map[string]any{}
 	res["code"] = code
@@ -25,4 +27,36 @@ func ReponseFormatWithMeta(code int, msg string, data any, meta any) (int, map[s
 	}
 
 	return code, res
+}
+
+func Pagination(limit, offset, totalData int) map[string]interface{} {
+	currentLimit := limit
+	if currentLimit <= 0 {
+		currentLimit = totalData
+	}
+
+	currentOffset := offset
+	if currentOffset <= 0 {
+		currentOffset = 0
+	}
+
+	currentPage := 1
+	if limit > 0 {
+		currentPage = (offset / limit) + 1
+	}
+
+	totalPage := 1
+	if limit > 0 {
+		totalPage = int(math.Ceil(float64(totalData) / float64(limit)))
+	}
+
+	pagination := map[string]interface{}{
+		"current_limit":  currentLimit,
+		"current_offset": currentOffset,
+		"current_page":   currentPage,
+		"total_data":     totalData,
+		"total_page":     totalPage,
+	}
+
+	return pagination
 }
