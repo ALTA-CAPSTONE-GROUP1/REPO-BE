0a6b83126103fce1feb10fc865e0a71c31697c9b
Kristain Putra <kputrapar@gmail.com>
2023-05-19 00:01:10 +0700

    feat(GetCcs entity): entities

--- feature/user/cc/repository/query.go
+++ feature/user/cc/repository/query.go
@@ -17,17 +17,25 @@ func New(db *gorm.DB) cc.Repository {
 	}
 }
 
-func (cm *ccModel) GetAllCc(userID string) (cc.CcCore, error) {
+func (cm *ccModel) GetAllCc(userID string) ([]cc.CcCore, error) {
 	var result []cc.CcCore
 	var ccsOwned []subRepo.Cc
 	var submissions []subRepo.Cc
 
 	if err := cm.db.Where("user_id = ?", userID).Find(&ccsOwned).Error; err != nil {
 		log.Errorf("error on finding cc by userid %w", err)
-		return cc.CcCore{}, err
+		return []cc.CcCore{}, err
 	}
 
-	for _, ccOwned := range ccsOwned{
-		if err := cm.db.Where("id = ?", ccOwned)
+	if err := cm.db.Where("user_id = ?", userID).
+		Preload("Files").
+		Preload("Tos").
+		Preload("Ccs").
+		Preload("Signs").
+		Find(&submissions).
+		Error; err != nil {
+		log.Errorf("error on finding submissions for user %s: %v", userID, err)
+		return []cc.CcCore{}, err
 	}
+	
 }
